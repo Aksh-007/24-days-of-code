82fd8ceb2c7c25665177b5d6ccee2e12a56664c1
Aksh-007 <akshaybarapatre54@gmail.com>
2023-04-20 13:26:07 +0530

    Day 1 task complete

--- Day1.js
+++ Day1.js
@@ -15,7 +15,18 @@ function removeDuplicateCharacters() {
     return result;
 }
 console.log(removeDuplicateCharacters());
-
+/**
+ * 
+ * @param {str} str 
+ * @returns uniqueChar []
+ * 
+ * step 1 - converting the string into array of elmenets using split()
+ * step 2  - creating uniqueChar [] empty array to store the unique char 
+ * step 3 -  looping over the string Arrays 
+ * step 4 -  condition if element not includes  in uniqueChar then push it in array
+ * step 5 -  converting a array back to string using join(';) method
+ * step 6 - returning the string back and console loging it 
+ */
 
 // creating a function for unique string
 let uniqueString = (str) => {
@@ -40,6 +51,61 @@ let uniqueString = (str) => {
 
 }
 console.log(uniqueString('akshay'));
+
+
+// Remove Duplicate characters from array of element and find the count of an elements using set
+
+/**
+ * 
+ * @param {array} arr 
+ * @returns {countOfElement, unique}
+ * step 1 - using set method to remove dplicate elemens from array
+ * step 2 - 
+ */
+
+const removeDuplicateElement = (arr) =>{
+    let uniqueElement = new Set(arr);
+    let countOfElement = uniqueElement.size
+    // here converting object into array
+    let unique = Array.from(uniqueElement);
+    return{ countOfElement, unique};
+}
+let {countOfElement, unique} = removeDuplicateElement([50,84,2,5,50,5,2,84])
+
+
+
+console.log(`Element in Array is ${countOfElement}`);
+console.log(` Array is ${unique}`);
+
+
+//Remove Duplicate characters from array of element using filter
+
+let functionFilter = (arr) =>{
+    let result = arr.filter((value, index, arr) => arr.indexOf(value) === index);
+    return result
+}
+
+console.log(functionFilter(['a', 1,2,3,'b',4,'a',1,2,'b']))
+// output // ['a', 1, 2, 3, 'b', 4]
+
+
+
+// String reverse without reversing of individual words (Array of elements can be reverse with reverse() method but for string it is won't possible so required to split and then join
+
+
+let reverseStringUsingArray = (str) =>{
+        let Array= str.split('');
+        return Array.reverse().join('')
+}
+
+console.log(reverseStringUsingArray("akshay afnf fnpfn npsfns"))
+
+
+
+
+
+
+
 
 
 
