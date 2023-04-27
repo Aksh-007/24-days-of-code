40d76d133e94532feb8c3f40f71e9710421e0e3d
Aksh-007 <akshaybarapatre54@gmail.com>
2023-04-27 14:02:34 +0530

    added find vowels in given string

--- Day4.js
+++ Day4.js
@@ -49,7 +49,7 @@ const longestWordFor = (input) => {
     let longestWord = '';
     inputLength = input.length
     for (let i = 0; i < inputLength; i++) {
-        if(input[i].length >= longestWord.length){
+        if (input[i].length >= longestWord.length) {
             longestWord = input[i];
         }
     }
@@ -60,8 +60,66 @@ longestWordFor("akshay is a good prorammer")
 
 // Q2. To find longest common string from array of strings
 
+/**
+ * 
+ * @param {input} input 
+ * @returns longestCommonWord
+ * 
+ * step 1 - 
+ */
+function longestCommonString() {
+    array = ["go", "google", "gosh"];
+    var arr = array.sort();
+    var i;
+    for (i = 0; i < arr[0].length && arr[0].charAt(i) === arr[arr.length - 1].charAt(i); i++);
+    console.log(arr[0].substring(0, i)); // "go"
+    return arr[0].substring(0, i);
+}
+
+longestCommonString();
 
 // Q3. To find vowels and its count in a given string
 
+const vowels = (input) => {
+    let vowels = {
+        a: 0,
+        e: 0,
+        i: 0,
+        o: 0,
+        u: 0,
+    };
+
+    for (let character of input) {
+        switch (character.toLowerCase()) {
+            case 'a':
+                vowels.a++;
+                break;
+
+            case 'e':
+                vowels.e++;
+                break;
+
+            case 'i':
+                vowels.i++;
+                break;
+
+            case 'o':
+                vowels.o++;
+                break;
+
+
+            case 'u':
+                vowels.u++;
+                break;
+
+
+            default:
+                break;
+        }
+    }
+    return vowels
+}
+
+vowels("akshay is a good programmer");
 
 // Q4. To find character occurance from the string
